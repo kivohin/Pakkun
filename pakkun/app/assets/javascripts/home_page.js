8516b0464d1f50298471b484d109d62300611b75
Anthony Nabuurs <kivohin@protonmail.com>
2016-05-18 17:55:16 +0200

    Erase Mapbox GL JS (fuck you for your bad documentation) from project, and substitute it with Mapbox.js. Map is able to display hard coded markers with tooltip

--- pakkun/app/assets/javascripts/home_page.js
+++ pakkun/app/assets/javascripts/home_page.js
@@ -3,16 +3,66 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(function() {
-	mapboxgl.accessToken = 'pk.eyJ1Ijoia2l2b2hpbiIsImEiOiJjaW83YW5vdTYwMDc5dW1rbjFtYzhqNTlvIn0.2fhQAfE3PKSQowqYxRJOTw';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/outdoors-v9',
-    center: [6.0044086, 51.8710129], // [lng, lat]
-    zoom: 5,
-    hash: false
-  })
+	// Provide your access token
+	L.mapbox.accessToken = 'pk.eyJ1Ijoia2l2b2hpbiIsImEiOiJjaW83YW5vdTYwMDc5dW1rbjFtYzhqNTlvIn0.2fhQAfE3PKSQowqYxRJOTw';
+	
+	// Create a map in the div #map
+	var map = L.mapbox.map('map', 'mapbox.outdoors', {
+		center: [51.8710129, 6.0044086], // [lat, lng]
+		zoom: 6,
+		zoomControl: false
+	});
 
-  map.addControl(new mapboxgl.Navigation({position: 'bottom-right'}));
-  map.addControl(new mapboxgl.Geolocate({position: 'bottom-right'}));
-  map.scrollZoom.disable();
+	L.control.zoom({
+		position: 'bottomright'
+	}).addTo(map);
+		
+	map.addControl(L.mapbox.geocoderControl('mapbox.places', {
+    autocomplete: true,
+    position : 'topleft',
+    // keepOpen: true
+  }));
+
+  map.scrollWheelZoom.disable();
+
+  L.control.locate({
+  	position : 'bottomright'
+  }).addTo(map);
+
+  var geojson = [
+	  {
+	    "type": "Feature",
+	    "geometry": {
+	      "type": "Point",
+	      "coordinates": [6.0044086, 51.8710129] // [lng, lat]
+	    },
+	    "properties": {
+	    	"title": "Venus Flytrap",
+      	"description": 'Croatan National Forest, North Carolina (USA) <a href="https://en.wikipedia.org/wiki/Venus_flytrap">Details</a>',
+	      "marker-color": "#DB3742",
+	      "marker-size": "large",
+	      "marker-symbol": "garden"
+	    }
+	  },
+	  {
+	    "type": "Feature",
+	    "geometry": {
+	      "type": "Point",
+	      "coordinates": [-122.413682,37.775408]
+	    },
+	    "properties": {
+	    	"title": "Sweet Pitcher Plant",
+      	"description": "Croatan National Forest, North Carolina (USA)",
+	      "marker-color": "#63b6e5",
+	      "marker-size": "large",
+	      "marker-symbol": "garden"
+	    }
+	  }
+	];
+
+	var myLayer = L.mapbox.featureLayer().setGeoJSON(geojson).addTo(map);
+
+	myLayer.on('click', function(e) {
+  	map.panTo(e.layer.getLatLng());
+  });
 });
