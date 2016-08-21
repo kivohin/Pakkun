4271c6f07f05a5eed4f45d12595ff50e5d8b1277
Anthony Nabuurs <kivohin@protonmail.com>
2016-08-20 23:58:05 -0400

    Create user_params action in Users controller for security in form.

--- pakkun/app/controllers/users_controller.rb
+++ pakkun/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+	private
+
+	def user_params
+		params.require(:user).permit(
+			:username,
+			:email,
+			:password,
+			:password_confirmation)
+	end
 end
