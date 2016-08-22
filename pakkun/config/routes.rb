9b7349059bd0247cbc28dac4ef83ce36e75efb2c
Anthony Nabuurs <kivohin@protonmail.com>
2016-08-21 23:03:34 -0400

    Add default routes for Users.

--- pakkun/config/routes.rb
+++ pakkun/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'plant_locations#index'
-  # get 'home' => 'plant_locations#index'
   resources :plant_locations
-  
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
