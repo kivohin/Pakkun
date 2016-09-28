1004f2eba9a08e50a810e53fecf889fe075832a2
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-27 21:06:17 -0400

    Add login route.

--- pakkun/config/routes.rb
+++ pakkun/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
 
+  get 'login' => 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
