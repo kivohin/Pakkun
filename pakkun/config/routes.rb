6c96a6f7e13425a155ac950d58971be2e686f4b7
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-27 21:14:51 -0400

    Add route for user login post request which links to the create action in sessions controller.

--- pakkun/config/routes.rb
+++ pakkun/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
 
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
