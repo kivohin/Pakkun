aabf44508a7604d83f1786ce9af889ca2d5cc604
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-27 20:58:16 -0400

    Modify in users controller create and user_params actions, delete show action.

--- pakkun/app/controllers/users_controller.rb
+++ pakkun/app/controllers/users_controller.rb
@@ -4,27 +4,19 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		user = User.new(user_params)
-		if user.save
-			redirect_to '/plant_locations'
-
+		@user = User.new(user_params)
+		
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to '/'
 		else
-			redirect_to 'users/new'
+			redirect_to '/signup'
 		end
 	end
 
-	def show
-		url_id = params[:id].to_i
-		@user = User.find_by(id: url_id)
-	end
-
 	private
-
+	
 	def user_params
-		params.require(:user).permit(
-			:username,
-			:email,
-			:password,
-			:password_confirmation)
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
